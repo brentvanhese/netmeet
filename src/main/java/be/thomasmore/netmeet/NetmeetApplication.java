cad579919fde79a095e7bf77693ae05be5db5417
brentvanhese <r0903817@student.thomasmore.be>
2022-03-01 15:15:04 +0100

    First commit

---
+++ src/main/java/be/thomasmore/netmeet/NetmeetApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.netmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetmeetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetmeetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/thomasmore/netmeet/NetmeetApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.netmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetmeetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
