1f1ef45a1b485cd256f8c56e5082df2a11eb7f02
brentvanhese <r0903817@student.thomasmore.be>
2022-03-04 14:11:19 +0100

    add atributes netwerkevent

--- src/main/java/be/thomasmore/netmeet/models/Netwerkevent.java
+++ src/main/java/be/thomasmore/netmeet/models/Netwerkevent.java
@@ -7,7 +7,65 @@ import javax.persistence.Id;
 public class Netwerkevent {
     @Id
     private Integer id;
+    private String naamNetwerkEvent;
+    private String provincie;
+    private String adres;
+    private String organisator;
+    private String info;
+    private int maxAanwezigen;
 
     public Netwerkevent() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNaamNetwerkEvent() {
+        return naamNetwerkEvent;
+    }
+
+    public void setNaamNetwerkEvent(String naamNetwerkEvent) {
+        this.naamNetwerkEvent = naamNetwerkEvent;
+    }
+
+    public String getProvincie() {
+        return provincie;
+    }
+
+    public void setProvincie(String provincie) {
+        this.provincie = provincie;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    public String getOrganisator() {
+        return organisator;
+    }
+
+    public void setOrganisator(String organisator) {
+        this.organisator = organisator;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public int getMaxAanwezigen() {
+        return maxAanwezigen;
+    }
+
+    public void setMaxAanwezigen(int maxAanwezigen) {
+        this.maxAanwezigen = maxAanwezigen;
     }
 }
