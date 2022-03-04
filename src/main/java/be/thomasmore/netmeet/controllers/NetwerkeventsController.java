1286ae10087fafd3f56afb17bbfa3fbe068be2d8
brentvanhese <r0903817@student.thomasmore.be>
2022-03-04 19:10:36 +0100

    add data to data.sql

--- src/main/java/be/thomasmore/netmeet/controllers/NetwerkeventsController.java
+++ src/main/java/be/thomasmore/netmeet/controllers/NetwerkeventsController.java
@@ -1,14 +1,22 @@
 package be.thomasmore.netmeet.controllers;
 
+import be.thomasmore.netmeet.models.Netwerkevent;
+import be.thomasmore.netmeet.repositories.NetwerkeventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class NetwerkeventsController {
+    @Autowired
+    private NetwerkeventRepository netwerkeventRepository;
+
     @GetMapping("/netwerkeventsList")
     public String netwerkeventsList(Model model){
-
+        Iterable<Netwerkevent> allNetwerkevents = netwerkeventRepository.findAll();
+        model.addAttribute("netwerkevents", allNetwerkevents);
+        model.addAttribute("nrVenues", netwerkeventRepository.count());
         return "netwerkeventsList";
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,29 @@
+INSERT INTO netwerkevent(
+    ID, NAAM_NETWERK_EVENT, VAK_GEBIED, PROVINCIE,
+    ADRES, ORGANISATOR, INFO, MAX_AANWEZIGEN)
+VALUES(
+          1, 'Java Antwerp meet', 'Java', 'Antwerpen', 'straat 1', 'Organisator 1', 'Netwerkevent voor Java', 50);
+
+INSERT INTO netwerkevent(
+    ID, NAAM_NETWERK_EVENT, VAK_GEBIED, PROVINCIE,
+    ADRES, ORGANISATOR, INFO, MAX_AANWEZIGEN)
+VALUES(
+          2, 'Python meet ', 'Python', 'Oost-vlaanderen', 'straat 2', 'Organisator 2', 'Netwerkevent voor Phyton', 75);
+
+INSERT INTO netwerkevent(
+    ID, NAAM_NETWERK_EVENT, VAK_GEBIED, PROVINCIE,
+    ADRES, ORGANISATOR, INFO, MAX_AANWEZIGEN)
+VALUES(
+          3, 'Webdesign meet', 'Webdesign', 'West-Vlaanderen', 'straat 3', 'Organisator 3', 'Netwerkevent voor webdesign', 30);
+
+INSERT INTO netwerkevent(
+    ID, NAAM_NETWERK_EVENT, VAK_GEBIED, PROVINCIE,
+    ADRES, ORGANISATOR, INFO, MAX_AANWEZIGEN)
+VALUES(
+          4, 'Databank meet', 'Databanken', 'Limburg', 'straat 4', 'Organisator 4', 'Netwerkevent voor databanken', 45);
+
+INSERT INTO netwerkevent(
+    ID, NAAM_NETWERK_EVENT, VAK_GEBIED, PROVINCIE,
+    ADRES, ORGANISATOR, INFO, MAX_AANWEZIGEN)
+VALUES(
+          5, 'Java Vlaam-Brabant meet', 'Java', 'Vlaams-Brabant', 'straat 5', 'Organisator 5', 'Netwerkevent voor Java', 100);
